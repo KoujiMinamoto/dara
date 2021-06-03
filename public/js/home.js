5972c938b3062ad7fc762b3196a00573530d0d2a
Kouji <sy135673@gmail.com>
2021-06-03 16:38:03 +1000

    base done

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,6 @@
 
 function add_job() {
+  
   var add_job= document.getElementById('Job');
   var nodeFather = add_job.parentNode;
       var node_clone = add_job.cloneNode();
@@ -7,4 +8,75 @@ function add_job() {
      node_clone.removeAttribute('id');
      node_clone.innerHTML = content;
     nodeFather.appendChild(node_clone);
+    console.log("Hello world!");
+    // getjobs();
+}
+
+function getjobs(){
+    // alert("CSS code: ");
+    var job= document.getElementById('Job');
+    $.ajax({
+        url: './dashboard/getallJobs',
+        type: "GET",
+        dataType: "JSON",
+        success: function (response) {
+            var info = JSON.stringify(response);
+            var data = eval('(' + info + ')');
+            console.log(response.length);
+            // $.each(data, function(index, item) {
+            //     // console.log(item.index);
+            //     $('#Job.form-control').append('<option value="' + item.index + '">' + item.key + '</option>');
+            //     });
+            for(i = 0; i < response.length; i++){
+                $('#Job.form-control').append('<option value="' + response[i].JOBID+ '">' + response[i].JOBID + '</option>');
+            }
+           
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert('Error '+xhr.status+' | '+thrownError);
+        },
+    });
+}
+
+function getcost(){
+    // $(this).parentNode.costcentre.append('<option value="' + response[i].CostCenterID+ '">' + response[i].CostCenterID + '</option>');
+    var options=$("#Job option:selected"); 
+    var obj = Element;
+    var abc = document.getElementById("Job");
+    var index = abc.selectedIndex; // 选中索引
+
+    // var text = abc.options[index].text; // 选中文本
+    var job = options.val();
+    // var value = abc.options[index].value; // 选中值
+    // console.log(options.val());
+    // $('#costcentre.form-control').append('<option value="' + 0 + '">' + "response[i].CostCenterID" + '</option>');
+    $.ajax({
+        url: './dashboard/getCostCentre/'+job,
+        type: "GET",
+        dataType: "JSON",
+        success: function (response) {
+            var info = JSON.stringify(response);
+            var data = eval('(' + info + ')');
+            // console.log(response.length);
+            // $.each(data, function(index, item) {
+            //     // console.log(item.index);
+            //     $('#Job.form-control').append('<option value="' + item.index + '">' + item.key + '</option>');
+            //     });
+            for(i = 0; i < response.length; i++){
+                $('#costcentre.form-control').append('<option value="' + response[i].CostCenterID+ '">' + response[i].CostCenterID + '</option>');
+            }
+           
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert('Error '+xhr.status+' | '+thrownError);
+        },
+    });
+}
+
+function initPage() {
+    // getjobs();
+}
+
+window.onload=function(){
+    getjobs();
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="https://static.pdflibr.com/SuperSlide/2.1.3/jquery.SuperSlide.2.1.3.js"></script>
+        
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" ></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script type="text/javascript"  src="{{URL::asset('/js/home.js') }}"></script>
+        <!-- <script src="https://code.jquery.com/jquery-3.2.1.min.js"> -->
     </head>
 
     <body>
@@ -16,20 +17,29 @@
                 <select class="form-control" id="Name">  
                 </select>
                 <button onclick="add_job()"></button>
-                <div id="Job">
+                <div class="Job" id="Job">
                     <p class="az-dashboard-text">Job Id</p>
-                    <select class="form-control" id="Job">  
+                    <select class="form-control" id="Job" onChange="getcost()">  
                     </select>
                     <p class="az-dashboard-text">Costcentre</p>
                     <select class="form-control" id="costcentre">  
                     </select>
                 </div>
                 
+                
             </div>
+            <button onclick="">submit</button>
         </div>
     </body>
-    <script type="text/javascript"  src="{{URL::asset('/js/main.js') }}></script>
+    
 
-
-
+    
+    <script type="text/javascript">
+    
+    // function getcost(){
+    //     var options=$("#Job option:selected"); 
+        
+    // }
+        
+    </script>
 </html>
