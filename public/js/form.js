20f2d1da67b88777978ed93aad7d67c89d6f4a3c
Kouji <sy135673@gmail.com>
2021-06-23 12:13:03 +1000

    test 1.1

--- public/js/form.js
+++ public/js/form.js
@@ -9,19 +9,30 @@ function submit(){
     var today = window.localStorage.date;
     var isoendtime = datetime.toISOString();
     var endtime = datetime.toISOString();
-    var Stringtime =window.localStorage.startStringtime  ;
+    var Stringtime =window.localStorage.startStringtime ;
+
     
     TimeStart = starttime.slice(0, 5);
     start = new Date(Stringtime);
     var diffmins = datetime.getMinutes() - start.getMinutes();
     var diffhours = datetime.getHours() - start.getHours();
     var totalmins = diffhours*60 + diffmins;
-    var spiltmins = diffmins/costs.length;
+    var sspiltmins = diffmins/costs.length;
+    if(sspiltmins<=15){
+        spiltmins = 15;
+    }else {
+        var ss = sspiltmins%15;
+        if(ss>7){
+            spiltmins = parseInt(sspiltmins/15)*15+15;
+        }else{
+            spiltmins = parseInt(sspiltmins/15)*15;
+        }
+    }
     
     
     for(i = 0; i < costs.length; i++){
         JobID= jobs[i];
-        StaffID = id[i];
+        StaffID = parseInt(id);
         SectionID = section[i];
         CostcentreID= costs[i];
         var start_time = new Date(Stringtime);
@@ -30,13 +41,37 @@ function submit(){
         end_time.setMinutes( start.getMinutes() + spiltmins*(i+1) );
         var finalstart = start_time.toTimeString().slice(0, 5);
         var finalend = end_time.toTimeString().slice(0, 5);
-        console.log(finalstart);
+        if(parseInt(start_time.toTimeString().slice(3, 5))<15){
+            var min  = "00";
+        }else if(parseInt(start_time.toTimeString().slice(3, 5))<30&&parseInt(start_time.toTimeString().slice(3, 5))>=15){
+            var min  = 15;
+        }else if(parseInt(start_time.toTimeString().slice(3, 5))<45&&parseInt(start_time.toTimeString().slice(3, 5))>=30){
+            var min  = 30;
+        }else{
+            var min  = 45;
+        }
+
+        if(parseInt(end_time.toTimeString().slice(3, 5))<15){
+            var emin  = "00";
+        }else if(parseInt(end_time.toTimeString().slice(3, 5))<30&&parseInt(end_time.toTimeString().slice(3, 5))>=15){
+            var emin  = 15;
+        }else if(parseInt(end_time.toTimeString().slice(3, 5))<45&&parseInt(end_time.toTimeString().slice(3, 5))>=30){
+            var emin  = 30;
+        }else{
+            var emin  = 45;
+        }
+        finalstart = start_time.toTimeString().slice(0, 3)+min;
+        finalend = end_time.toTimeString().slice(0, 3)+emin;
+
+            
+        console.log(StaffID);
 
         $.ajax({
             url: "https://uat-daraswitchboards.simprosuite.com/api/v1.0/companies/0/jobs/"+JobID+"/sections/"+SectionID+"/costCenters/"+CostcentreID+"/schedules/",
             type: "POST",
             dataType: "JSON",
             headers: {
+                "Content-Type": "application/json",
                 "Authorization": "Bearer 36c519f7b6e3aa89722e954bb7057592992fc092"
             },
             data: JSON.stringify({
@@ -57,7 +92,7 @@ function submit(){
                 console.log(response);
             },
             error: function (xhr, ajaxOptions, thrownError) {
-                alert('Error '+xhr.status+' | '+thrownError);
+                alert('A schedule for this job on this date already exists. '+xhr.status+' | '+thrownError);
             },
         });
         
